0d7ace332fa003a76a1798d3f12eb299b2f9b95d
Shechang Li <hcydlee@gmail.com>
2014-06-09 01:12:42 +0800

    Quiz1

---
+++ quiz1.R
@@ -0,0 +1,26 @@
+#1
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv",
+              destfile='Idaho',
+              method="internal" )
+idaho<-read.csv("Idaho",header=TRUE)
+k<-idaho[idaho$TYPE==1&idaho$VAL==24,]
+table(k$RT)
+
+#3
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx",
+              destfile='gas.xlsx',
+              method="internal" )
+library(xlsx)
+dat<-read.xlsx("gas.xlsx",1,colIndex=7:15,rowIndex=18:23)
+sum(dat$Zip*dat$Ext,na.rm=T) 
+
+#4
+library(XML)
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml",
+              destfile='restaurants.xml',
+              method="internal" )
+doc<-xmlTreeParse('restaurants.xml')
+res<-xmlRoot(doc)
+xmlName(res)
+res[[1]][[2]]
+zip<-xpathSApply(res,"//zipcode",xmlValue)
